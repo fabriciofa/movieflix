5daca251922697f75bd078bec8b8293b67876000
Fabricio Araújo <fabriciofa@gmail.com>
2022-01-31 13:45:15 -0300

    movie entity and seed

--- backend/src/main/java/com/fabriciofa/movieflix/entities/Genre.java
+++ backend/src/main/java/com/fabriciofa/movieflix/entities/Genre.java
@@ -1,12 +1,15 @@
 package com.fabriciofa.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Genre implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@OneToMany(mappedBy = "genre")
+	private Set<Movie> movies = new HashSet<>();
 
 	public Genre() {
 	
@@ -42,6 +48,10 @@ public class Genre implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Movie> getMovies() {
+		return movies;
 	}
 
 	@Override
